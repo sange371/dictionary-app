3958aa15ba04b4d62a8309da592bab1675e9972e
sange371 <zusangembaleki@icloud.com>
2025-11-18 10:01:09 +0200

    added results

--- dict-app/src/Search.js
+++ dict-app/src/Search.js
@@ -4,15 +4,17 @@ import './Search.css';
 
 export default function Search(){
     let [value, setValue]=useState("");
+    let [result, setResult]=useState(null);
+
     
     function handleResponse(response){
-        console.log( response.data[0]);
+        setResult( response.data[0]);
     }
 
 
     function handleSubmit(event){
         event.preventDefault();
-        alert(`Searching for ${value}`);
+        
 
         let url=`https://api.dictionaryapi.dev/api/v2/entries/en/${value}`;
         axios.get(url).then(handleResponse);
@@ -29,7 +31,27 @@ export default function Search(){
                 <input type="search" placeholder="Type a word..." onChange={changeValue}/>
                 <input type="submit" value="Search"/>
             </form>
-            
+            {result && (
+                <div className="Results">
+                    <h2>{result.word}</h2>
+                    {result.meanings.map(function(meaning,index){
+                        return(
+                            <div key={index}>
+                                <h3>{meaning.partOfSpeech}</h3>
+                                {meaning.definitions.map(function(definition,index){
+                                    return(
+                                        <div key={index}>
+                                            <p>{definition.definition}</p>
+                                            <p><em>{definition.example}</em></p>
+                                            </div>
+                                        
+                                    )
+                                })}
+                            </div>
+                        )
+                    })}
+                </div>
+            )}
         </div>
     )
 }
