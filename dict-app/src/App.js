5d1dd5b71177bb31cfbefe4042e5afb2fb97967c
sange371 <zusangembaleki@icloud.com>
2025-11-18 20:43:12 +0200

    added photos and an audio

--- dict-app/src/App.js
+++ dict-app/src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Dictionary App</h1>
       <Search />
     </div>
   );
--- dict-app/src/Search.js
+++ dict-app/src/Search.js
@@ -1,16 +1,22 @@
 import React,{useState} from 'react';
 import axios from 'axios';
+import ReactAudioPlayer from 'react-audio-player';
 import './Search.css';
 
 export default function Search(){
     let [value, setValue]=useState("");
     let [result, setResult]=useState(null);
+    let [photos, setPhotos]=useState(null);
 
     
     function handleResponse(response){
         setResult( response.data[0]);
     }
 
+    function handlePexelsResponse(response){
+        setPhotos(response.data.photos);
+
+    }
 
     function handleSubmit(event){
         event.preventDefault();
@@ -18,6 +24,14 @@ export default function Search(){
 
         let url=`https://api.dictionaryapi.dev/api/v2/entries/en/${value}`;
         axios.get(url).then(handleResponse);
+
+        let pexelsApiKey="Q3JFvcSE4jsCAkUWBOr1j3fNJKuakT10LgXACNfA3BuGNwdkd2YzA2yt";
+        let pexelsUrl=`https://api.pexels.com/v1/search?query=${value}&per_page=1`;
+
+        let header = { Authorization: pexelsApiKey };
+        axios.get(pexelsUrl, { headers: header }).then(handlePexelsResponse);
+
+    
     }
 
 
@@ -27,13 +41,22 @@ export default function Search(){
 
     return(
         <div className="Search">
+            <h1>Dictionary App</h1>
             <form onSubmit={handleSubmit}>
-                <input type="search" placeholder="Type a word..." onChange={changeValue}/>
-                <input type="submit" value="Search"/>
+                <input type="search" placeholder="Type a word..." className="searchEngine"onChange={changeValue} autoFocus/>
+                <input type="submit" value="Search" className="button"/>
             </form>
             {result && (
                 <div className="Results">
                     <h2>{result.word}</h2>
+                    {result.phonetics.map(function(phonetic,index){
+                        return(
+                            <div key={index}>
+                                <p className="phonetic">{phonetic.text}</p>
+                                <ReactAudioPlayer src={phonetic.audio} controls  className="audio" autoPlay={false}/>
+                            </div>
+                        )
+                    })}
                     {result.meanings.map(function(meaning,index){
                         return(
                             <div key={index}>
@@ -41,15 +64,38 @@ export default function Search(){
                                 {meaning.definitions.map(function(definition,index){
                                     return(
                                         <div key={index}>
-                                            <p>{definition.definition}</p>
-                                            <p><em>{definition.example}</em></p>
-                                            </div>
-                                        
-                                    )
+                                            <p className="definition"><strong>Definition:</strong> {definition.definition}</p>
+                                            {definition.example &&(
+                                                <p className="example"><em><strong>Example:</strong> {definition.example}</em></p>
+                                            )}
+                                            
+                                            {meaning.synonyms && meaning.synonyms.length >0 && (
+                                                <div className="synonyms">
+                                                    <p><strong>Synonyms:</strong></p>
+                                                    <ul>
+                                                        {meaning.synonyms.map(function(synonym,index){
+                                                            return(                                                        
+                                                                <li key={index}><strong><em><small>{synonym}</small></em></strong></li>
+                                                           )
+                                                        })}
+                                                    </ul>
+                                                </div>
+                                            )}   
+                                        </div>    
+                                    );
                                 })}
                             </div>
-                        )
+                        );
                     })}
+                    {photos &&(
+                        <div className="Photos">
+                            {photos.map(function(photo,index){
+                                return(
+                                    <img key={index} src={photo.src.medium} alt={value} className="img-fluid"/>
+                                )
+                            })}
+                        </div>
+                    )}
                 </div>
             )}
         </div>
