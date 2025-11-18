b36c01af8ba031b9b3b4895d5d1fcda8ddcef6d7
sange371 <zusangembaleki@icloud.com>
2025-11-18 07:45:48 +0200

    added useState and event listeners

--- dict-app/src/Search.js
+++ dict-app/src/Search.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './Search.css';
 
 export default function Search(){
+    let [value, setValue]=useState("");
+    
+
+
+    function handleSubmit(event){
+        event.preventDefault();
+        alert(`Searching for ${value}`);
+    }
+    function changeValue(event){
+        setValue(event.target.value);
+    }
+
     return(
         <div className="Search">
-            <form>
-                <input type="search" placeholder="Type a word..."/>
+            <form onSubmit={handleSubmit}>
+                <input type="search" placeholder="Type a word..." onChange={changeValue}/>
                 <input type="submit" value="Search"/>
             </form>
         </div>
