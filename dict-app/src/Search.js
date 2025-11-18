fd3301ba4425e1658ce4c4b06aec9f2e041a2f27
sange371 <zusangembaleki@icloud.com>
2025-11-18 08:33:16 +0200

    added api intergration

--- dict-app/src/Search.js
+++ dict-app/src/Search.js
@@ -1,15 +1,24 @@
 import React,{useState} from 'react';
+import axios from 'axios';
 import './Search.css';
 
 export default function Search(){
     let [value, setValue]=useState("");
     
+    function handleResponse(response){
+        console.log( response.data[0]);
+    }
 
 
     function handleSubmit(event){
         event.preventDefault();
         alert(`Searching for ${value}`);
+
+        let url=`https://api.dictionaryapi.dev/api/v2/entries/en/${value}`;
+        axios.get(url).then(handleResponse);
     }
+
+
     function changeValue(event){
         setValue(event.target.value);
     }
@@ -20,6 +29,7 @@ export default function Search(){
                 <input type="search" placeholder="Type a word..." onChange={changeValue}/>
                 <input type="submit" value="Search"/>
             </form>
+            
         </div>
     )
 }
